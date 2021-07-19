077643ba1f49aee3762616673635fbe7e3131953
Krshivam25 <49578630+Krshivam25@users.noreply.github.com>
2021-07-19 22:53:50 +0530

    String Compression

---
+++ Arrays and Strings/StringCompression.java
@@ -0,0 +1,33 @@
+
+
+public class StringCompression { 
+   public static String compression(String str) {
+       if(str.length() == 0){
+           return str;
+       }
+       StringBuilder sb = new StringBuilder(str.length() + 1);
+       char currChar = str.charAt(0);
+       int currCount = 1;
+       for(int i=1; i<str.length(); i++) {
+           if(str.charAt(i) == currChar) {
+               currCount++;
+           }
+           else {
+               sb.append(currChar);
+               sb.append(currCount);
+               if(sb.length() > str.length()) {
+                   return str;
+               }
+               currChar = str.charAt(i);
+               currCount = 1;
+
+           }
+       }
+       sb.append(currChar);
+       sb.append(currCount);
+       return sb.toString();
+   }
+ public static void main(String[] args) {
+     System.out.println(compression("aabbccddeef"));
+ }      
+}
