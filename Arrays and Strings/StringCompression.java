4cb1911e33f0a2ef5c2c44704260f0dd40a3756e
Krshivam25 <49578630+Krshivam25@users.noreply.github.com>
2021-07-20 16:51:15 +0530

    String Compression

--- Arrays and Strings/StringCompression.java
+++ Arrays and Strings/StringCompression.java
@@ -1,3 +1,5 @@
+// Implement a method to perform basic string compression using the counts of repeated characters. 
+// For example, the string aabccccaaa would become a2b1c5a3.
 
 
 public class StringCompression { 
