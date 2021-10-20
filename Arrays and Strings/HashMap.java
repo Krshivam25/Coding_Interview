297875b954d1354ef8e3946c5d5f975125c79a04
Krshivam25 <49578630+Krshivam25@users.noreply.github.com>
2021-10-20 11:29:00 +0530

    Valid paran

--- Arrays and Strings/HashMap.java
+++ Arrays and Strings/HashMap.java
@@ -1,31 +1,29 @@
 import java.util.*;
+import java.util.HashMap;
 
 public class HashMap {
     public static void main(String[] args) {
-        HashMap<String, Integer>map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         print(map);
         map.put("abc", 10);
         map.put("def", 15);
         map.put("xyz", 30);
 
-        System.out.println("Size of map is: " +map.size());
-        if(map.containsKey("abc")) {
+        System.out.println("Size of map is: " + map.size());
+        if (map.containsKey("abc")) {
             Integer a = map.get("abc");
-            System.out.println("Value of Key \"abc\" is:- " +a);
-
+            System.out.println("Value of Key \"abc\" is:- " + a);
 
         }
         map.clear();
 
         print(map);
     }
-    public static void print(Map<String,Integer>map)
-    {
-        if(map.isEmpty())
-        {
+
+    public static void print(Map<String, Integer> map) {
+        if (map.isEmpty()) {
             System.out.println("Map is Empty");
-        }
-        else{
+        } else {
             System.out.println(map);
         }
     }
